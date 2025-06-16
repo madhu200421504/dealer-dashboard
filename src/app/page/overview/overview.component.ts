4a25b1101a4c26a1741772e301b20e18cfe273fc
Madhu <madhumitha.mani@ariantechsolutions.com>
2025-06-16 14:49:50 +0530

    updated

--- src/app/page/overview/overview.component.ts
+++ src/app/page/overview/overview.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; 
-import { SharedModule } from '../../shared/shared.module'
-import { DashboardComponent } from "../dashboard/dashboard.component";
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [SharedModule, DashboardComponent], 
+  imports: [SharedModule, DashboardComponent],
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.css'
+  styleUrl: './overview.component.css',
 })
 export class OverviewComponent {
-
+  
 }
